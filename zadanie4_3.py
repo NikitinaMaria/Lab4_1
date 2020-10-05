a177870024a0a81e78eb6e7297f3d3e4ac7b9c48
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2020-10-05 09:47:11 +0300

    Add files via upload

--- zadanie4_3.py
+++ zadanie4_3.py
@@ -37,70 +37,111 @@ for i in range(255):
 # Grass
 rect(screen, (0, 255, 0), (0, 400, 600, 400))
 
-# Tree
-rect(screen_tree, (230, 230, 230), (110, 470, 30, 100))
-ellipse(screen_tree, (0, 128, 0), (45, 130, 160, 200))
-ellipse(screen_tree, (0, 255, 0), (44, 128, 164, 204), 5)
-ellipse(screen_tree, (0, 128, 0), (0, 270, 250, 140))
-ellipse(screen_tree, (0, 255, 0), (-1, 268, 253, 143), 5)
-ellipse(screen_tree, (0, 128, 0), (45, 370, 155, 115))
-ellipse(screen_tree, (0, 255, 0), (45, 369, 160, 118), 5)
-# White ellipses
-ellipse(screen_tree, (255, 204, 170), (15, 325, 45, 41))
-ellipse(screen_tree, (0, 255, 0), (15, 325, 45, 41), 5)
-ellipse(screen_tree, (255, 204, 170), (130, 150, 45, 40))
-ellipse(screen_tree, (0, 255, 0), (130, 150, 45, 40), 5)
-ellipse(screen_tree, (255, 204, 170), (190, 327, 47, 41))
-ellipse(screen_tree, (0, 255, 0), (190, 327, 47, 41), 5)
-ellipse(screen_tree, (255, 204, 170), (145, 435, 42, 42))
-ellipse(screen_tree, (0, 255, 0), (145, 435, 42, 42), 5)
+def draw_tree(screen_x, screen_y, x, y):
+
+    '''
+    Function draws tree
+    :param screen_x: tree width
+    :param screen_y: tree height
+    :param x, y: coordinates of the upper left corner
+    '''
+
+    # Tree
+    rect(screen_tree, (230, 230, 230), (110, 470, 30, 100))
+    ellipse(screen_tree, (0, 128, 0), (45, 130, 160, 200))
+    ellipse(screen_tree, (0, 255, 0), (44, 128, 164, 204), 5)
+    ellipse(screen_tree, (0, 128, 0), (0, 270, 250, 140))
+    ellipse(screen_tree, (0, 255, 0), (-1, 268, 253, 143), 5)
+    ellipse(screen_tree, (0, 128, 0), (45, 370, 155, 115))
+    ellipse(screen_tree, (0, 255, 0), (45, 369, 160, 118), 5)
+    # White ellipses
+    ellipse(screen_tree, (255, 204, 170), (15, 325, 45, 41))
+    ellipse(screen_tree, (0, 255, 0), (15, 325, 45, 41), 5)
+    ellipse(screen_tree, (255, 204, 170), (130, 150, 45, 40))
+    ellipse(screen_tree, (0, 255, 0), (130, 150, 45, 40), 5)
+    ellipse(screen_tree, (255, 204, 170), (190, 327, 47, 41))
+    ellipse(screen_tree, (0, 255, 0), (190, 327, 47, 41), 5)
+    ellipse(screen_tree, (255, 204, 170), (145, 435, 42, 42))
+    ellipse(screen_tree, (0, 255, 0), (145, 435, 42, 42), 5)
+
+    # Drawing tree
+    screen.blit(pygame.transform.scale(screen_tree, (screen_x, screen_y)), (x, y))
+    
+# Unicorn
+def unicorn():
+    '''
+    Function draws unicorn on a separate screen
+    '''
+
+    # Tail
+    for i in range(25):
+        ellipse(screen_unicorn, colors[randint(0, 4)], (
+        randint(288 - i * 4, 290 - i * 2), randint(580 + i * 5, 605 + i * 5), randint(30, 45 + 3 * i // 2),
+        randint(12, 27)))
+    # Body
+    ellipse(screen_unicorn, (255, 255, 255), (440, 470, 80, 60))
+    ellipse(screen_unicorn, (255, 255, 255), (470, 490, 80, 40))
+    ellipse(screen_unicorn, (255, 255, 255), (290, 565, 225, 110))
+    rect(screen_unicorn, (255, 255, 255), (445, 500, 60, 120))
+    # Legs
+    rect(screen_unicorn, (255, 255, 255), (305, 635, 18, 110))
+    rect(screen_unicorn, (255, 255, 255), (347, 635, 22, 100))
+    rect(screen_unicorn, (255, 255, 255), (430, 635, 20, 120))
+    rect(screen_unicorn, (255, 255, 255), (480, 635, 16, 95))
+    # Eye
+    ellipse(screen_unicorn, (229, 128, 255), (475, 485, 23, 20))
+    ellipse(screen_unicorn, (34, 28, 36), (485, 490, 9, 9))
+    ellipse(screen_ellipse, (255, 255, 255), (200, 320, 12, 7))
+    # Horn
+    polygon(screen_unicorn, (233, 175, 175), ([(485, 390), (485, 475), (465, 473), (485, 390)]))
+    # Mane
+    for i in range(14):
+        ellipse(screen_unicorn, colors[randint(0, 4)], (
+        randint(438 - i * 7, 440 - i * 2), randint(460 + i * 8, 474 + i * 7), randint(30, 45 + 3 * i // 2),
+        randint(15, 30)))
+
+    # Speck
+    screen_ellipse_2 = pygame.transform.rotate(screen_ellipse, 135)
+    screen_unicorn.blit(pygame.transform.scale(screen_ellipse_2, screen_unicorn.get_rect().size), (177, -8))
+
+def draw_unicorn(screen_x, screen_y, x, y):
+    '''
+    Function draws unicorn
+    :param screen_x: unicorn width
+    :param screen_y: unicorn height
+    :param x, y: coordinates of the upper left corner
+    '''
+
+    # Drawing unicorn
+    screen.blit(pygame.transform.scale(screen_unicorn, (screen_x, screen_y)), (x, y))
+
+def draw_mirrored_unicorn(screen_x, screen_y, x, y):
+    '''
+    Function draws mirrored unicorn
+    :param screen_x: unicorn width
+    :param screen_y: unicorn height
+    :param x, y: coordinates of the upper left corner
+    '''
+
+    unicorn()
+
+    # Mirrored unicorn
+    screen_unicorn_2 = pygame.transform.flip(screen_unicorn, True, False)
+    screen.blit(pygame.transform.scale(screen_unicorn_2, (screen_x, screen_y)), (x, y))
 
 # Drawing 5 trees
-screen.blit(pygame.transform.scale(screen_tree, (700, 900)), (50, -140))
-screen.blit(pygame.transform.scale(screen_tree, (450, 800)), (-50, 50))
-screen.blit(pygame.transform.scale(screen_tree, (450, 400)), (130, 270))
-screen.blit(pygame.transform.scale(screen_tree, (400, 500)), (50, 320))
-screen.blit(pygame.transform.scale(screen_tree, (400, 500)), (-30, 430))
-
-# Unicorn
-# Tail
-for i in range(25):
-    ellipse(screen_unicorn, colors[randint(0, 4)], (
-    randint(288 - i * 4, 290 - i * 2), randint(580 + i * 5, 605 + i * 5), randint(30, 45 + 3 * i // 2),
-    randint(12, 27)))
-# Body
-ellipse(screen_unicorn, (255, 255, 255), (440, 470, 80, 60))
-ellipse(screen_unicorn, (255, 255, 255), (470, 490, 80, 40))
-ellipse(screen_unicorn, (255, 255, 255), (290, 565, 225, 110))
-rect(screen_unicorn, (255, 255, 255), (445, 500, 60, 120))
-# Legs
-rect(screen_unicorn, (255, 255, 255), (305, 635, 18, 110))
-rect(screen_unicorn, (255, 255, 255), (347, 635, 22, 100))
-rect(screen_unicorn, (255, 255, 255), (430, 635, 20, 120))
-rect(screen_unicorn, (255, 255, 255), (480, 635, 16, 95))
-# Eye
-ellipse(screen_unicorn, (229, 128, 255), (475, 485, 23, 20))
-ellipse(screen_unicorn, (34, 28, 36), (485, 490, 9, 9))
-ellipse(screen_ellipse, (255, 255, 255), (200, 320, 12, 7))
-# Horn
-polygon(screen_unicorn, (233, 175, 175), ([(485, 390), (485, 475), (465, 473), (485, 390)]))
-# Mane
-for i in range(14):
-    ellipse(screen_unicorn, colors[randint(0, 4)], (
-    randint(438 - i * 7, 440 - i * 2), randint(460 + i * 8, 474 + i * 7), randint(30, 45 + 3 * i // 2),
-    randint(15, 30)))
-
-# Speck
-screen_ellipse_2 = pygame.transform.rotate(screen_ellipse, 135)
-screen_unicorn.blit(pygame.transform.scale(screen_ellipse_2, screen_unicorn.get_rect().size), (177, -8))
-# Mirrored unicorn
-screen_unicorn_2 = pygame.transform.flip(screen_unicorn, True, False)
+draw_tree(700, 900, 50, -140)
+draw_tree(450, 800, -50, 50)
+draw_tree(450, 400, 130, 270)
+draw_tree(400, 500, 50, 320)
+draw_tree(400, 500, -30, 430)
 
 # Drawing 4 unicorns
-screen.blit(pygame.transform.scale(screen_unicorn, (500, 680)), (-30, 130))
-screen.blit(pygame.transform.scale(screen_unicorn_2, (350, 500)), (400, 170))
-screen.blit(pygame.transform.scale(screen_unicorn, (250, 320)), (230, 170))
-screen.blit(pygame.transform.scale(screen_unicorn_2, (120, 150)), (500, 280))
+draw_mirrored_unicorn(350, 500, 400, 170)
+draw_unicorn(250, 320, 230, 170)
+draw_mirrored_unicorn(120, 150, 500, 280)
+draw_unicorn(500, 680, -30, 130)
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -111,3 +152,5 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+
+pygame.quit()
